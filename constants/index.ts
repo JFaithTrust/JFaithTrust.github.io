de5935da1504d622d0887fdaafa2a7225d5f1a88
JFaithTrust <jahongirsolijoniy@gmail.com>
2024-04-16 15:09:48 +0500

    finished

---
+++ constants/index.ts
@@ -0,0 +1,299 @@
+export const navItems = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  
+  export const Skill_data = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "CSS",
+      Image: "/css.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "JavaScript",
+      Image: "/javascript.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "TypeScript",
+      Image: "/typescript.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "React JS",
+      Image: "/react.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Redux Toolkit",
+      Image: "/redux.svg",
+      width: 60,
+      height: 60,
+    },
+  ];
+  
+  export const Frontend_skill = [
+    {
+      skill_name: "Material UI",
+      Image: "/material-ui.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Tailwind CSS",
+      Image: "/tailwind.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Vue JS",
+      Image: "/vue.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Zod",
+      Image: "/zod.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Shadcn/ui",
+      Image: "/shadcn-ui.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Vite JS",
+      Image: "/vite.svg",
+      width: 60,
+      height: 60,
+    },
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Chart js",
+      Image: "/chart.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Framer Motion",
+      Image: "/motion.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Three js",
+      Image: "/three-js.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Git",
+      Image: "/git.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Figma",
+      Image: "/figma.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Next JS",
+      Image: "/next-js.svg",
+      width: 80,
+      height: 60,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Vue.js Developer",
+      company_name: "uzcsd.uz",
+      icon: "/uzcsd.png",
+      iconBg: "#383E56",
+      date: "Sep 2022 - Feb 2023",
+      points: [
+        "The Central Securities Depository serves as a centralized platform for the storage and accounting of rights within the depository system of the Republic of Uzbekistan. By providing a unified and secure infrastructure, the organization facilitates the efficient management and tracking of securities, ensuring transparency and integrity in financial transactions across the country's capital markets.",
+        "With a commitment to investor protection and market integrity, the Central Securities Depository implements robust safeguards and regulatory frameworks to safeguard the interests of market participants.",
+      ],
+    },
+    {
+      title: "Vue.js Developer",
+      company_name: "depo.uzcsd.uz",
+      icon: "/uzcsd.png",
+      iconBg: "#E6DEDD",
+      date: "Feb 2023 - May 2023",
+      points: [
+        "Built with Vue 3, 'Depo.uzcsd.uz' prioritizes user experience by delivering a seamless and interactive browsing experience. The website's intuitive interface, responsive design, and optimized performance ensure that users can easily navigate, engage with content, and find the information they need, enhancing overall user satisfaction and engagement with the platform.",
+        "'Depo.uzcsd.uz' effectively organizes and presents news related to the company's depot operations in a clear and accessible manner. With dedicated sections for Depo News, Depo Vestnik, and Depo Compass, users can easily navigate and find relevant information, ensuring they stay informed about the latest developments and updates within the depot.",
+      ],
+    },
+    {
+      title: "React js Developer",
+      company_name: "Delixi Electrics",
+      icon: "/delixi.svg",
+      iconBg: "#383E56",
+      date: "October 2023 - November 2023",
+      points: [
+        "Delixi - Electric offers users a wealth of information about electrical products, ranging from specifications and features to usage tips and compatibility details. By providing comprehensive insights, users can make informed decisions when selecting electrical products for their specific needs.",
+        "Delixi - Electric plays a pivotal role in promoting awareness and appreciation for the electric industry. By showcasing the diversity and innovation within the sector, the platform highlights the significance of electrical products in everyday life and encourages engagement and interest among users, ultimately contributing to the advancement of the industry as a whole.",
+      ],
+    }, {
+      title: "Next js Developer",
+      company_name: "Conference-list.uz",
+      icon: "/conference.png",
+      iconBg: "#E6DEDD",
+      date: "December 2023 - February 2024",
+      points: [
+        "Easily create, schedule, and manage conferences, workshops, and seminars directly through the platform. From setting event dates to defining topics and inviting speakers, Conference-list.uz provides comprehensive tools for efficient event planning.",
+        "Ensure data privacy and security with robust encryption protocols and secure data storage mechanisms. Conference-list.uz prioritizes the protection of user information, maintaining compliance with industry standards and regulations.",
+      ],
+    },{
+      title: "React js Developer",
+      company_name: "Uzworks.uz",
+      icon: "/uzworks.svg",
+      iconBg: "#383E56",
+      date: "February 2024 - Present",
+      points: [
+        "Uzworks.uz leverages advanced algorithms to facilitate seamless job matching, enabling job seekers to discover relevant employment opportunities that align with their skills, experience, and career objectives. Employers can also efficiently find qualified candidates based on specific criteria.",
+        "The platform boasts an extensive database of job listings spanning various industries, sectors, and job roles across Uzbekistan. From entry-level positions to executive roles, Uzworks.uz caters to diverse job seekers and employers, ensuring comprehensive coverage of the job market.",
+      ],
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "uzcsd.uz",
+      description:
+        "Joint-Stock Company 'Central Securities Depository' is an organization that provides a unified system of storage and accounting of rights in the depository system of the Republic of Uzbekistan.",
+      tags: [
+        {
+          name: "vue 3",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "vue i18n",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: '/uzcsd.jpg',
+      source_code_link: "https://uzcsd.uz/",
+    },
+    {
+      name: "depo.uzcsd.uz",
+      description:
+        "'Depo.uzcsd.uz' is a website based on Vue 3, which effectively serves as a platform for displaying news about the company's depot. The site has three main categories: Depo News, Depo Vestnik and Depo Compass",
+      tags: [
+        {
+          name: "vue 3",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "vue i18n",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: '/depo.jpg',
+      source_code_link: "https://depo.uzcsd.uz/",
+    },
+    {
+      name: "Delixi - Electric",
+      description:
+        "Delixi - Electric is a React project that offers valuable information about electric products. It is a promising platform dedicated to empowering users with insights into the world of electrical products.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "framer-motion",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: '/delixiImage.jpg',
+      source_code_link: "https://github.com/JFaithTrust/delixi",
+    },
+    {
+      name: "Conference-list.uz",
+      description:
+          "Conference-list.uz revolutionizes conference management within universities, empowering organizers to create memorable and impactful events while providing participants with a seamless and engaging experience.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "next js",
+          color: "green-text-gradient",
+        },
+        {
+          name: "shadcn/ui",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: '/conferenceImage.jpg',
+      source_code_link: "https://conference-app-delta.vercel.app/",
+    },
+    {
+      name: "Uzworks.uz",
+      description:
+          "Uzworks.uz emerges as a game-changer in the Uzbekistan job market, offering a cutting-edge platform that empowers individuals and businesses to thrive in today's competitive landscape.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "shadcn/ui",
+          color: "green-text-gradient",
+        },
+        {
+          name: "vite js",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: '/uzWorksImage.jpg',
+      source_code_link: "https://www.uzworks.uz/",
+    },
+  ];
+  
+  
+  export { experiences, projects };
